a029d696cc2cb352ee611eb9a126da2870981996
Duc <nguyenduck2809@gmail.com>
2024-09-27 21:06:21 +0700

    a

--- script/script.js
+++ script/script.js
@@ -9,3 +9,43 @@ openvideo.addEventListener("click",()=>{
 closevideo.addEventListener("click",()=>{
     videomodal.style.visibility = "hidden"
 })
+
+var image_list = document.getElementsByClassName("nav-pane-image")
+
+for(var i=0 ; i<image_list.length ; i++){
+    (function(index){
+        image_list[index].addEventListener("click",()=>{
+            resetImageNavPane()
+            image_list[index].src = "image/nav_ck.png"
+        })
+    })(i)
+}
+
+// Reset all image to nav_hover
+function resetImageNavPane(){
+    var image_list = document.getElementsByClassName("nav-pane-image")
+    for(var i=0 ; i<image_list.length ; i++){
+        image_list[i].src = "image/nav_hover.png"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
